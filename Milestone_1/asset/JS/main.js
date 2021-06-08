9944f279a47611fc308f7ba064e5b3994186fe4b
planeset94 <planeset94@gmail.com>
2021-06-08 13:17:53 +0200

    M1: termined. added search bar & splitted url

--- Milestone_1/asset/JS/main.js
+++ Milestone_1/asset/JS/main.js
@@ -4,30 +4,40 @@ const app = new Vue(
 
         data: {
             errore: '',
-            url: 'https://api.themoviedb.org/3/search/multi?api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=falcon&include_adult=false',
+            userSearch: '',
+            partialUrl: 'https://api.themoviedb.org/3/search/multi?api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=',
             arraySearch: [],
+            visible: true,
 
         },
 
         methods: {
+            callApi() {
 
+                if (this.userSearch == '') {
+                    this.visible = false
+                } else {
+                    this.visible = true
+                    axios
+                        .get(this.partialUrl + this.userSearch)
+                        .then(resp => {
+                            // console.log(resp.data.results);
+                            this.arraySearch = resp.data.results
+                            console.log(this.arraySearch);
+                        })
+
+                        .catch(e => {
+                            this.errore = `Huston, We Have a Problem! --- ${e} `
+                        })
+
+
+                }
+
+            },
 
         },
 
-
-
         mounted() {
-            axios
-                .get(this.url)
-                .then(resp => {
-                    // console.log(resp.data.results);
-                    this.arraySearch = resp.data.results
-                    console.log(this.arraySearch);
-                })
-
-                .catch(e => {
-                    this.errore = `Huston, We Have a Problem! --- ${e} `
-                })
 
 
 
