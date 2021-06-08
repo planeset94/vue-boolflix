d63cd4c566d8590ceae4529da49ac1a045840e51
planeset94 <planeset94@gmail.com>
2021-06-08 12:14:50 +0200

    M1: linked VUEJs & axios CDN (from API to array)

--- Milestone_1/asset/JS/main.js
+++ Milestone_1/asset/JS/main.js
@@ -1,1 +1,56 @@
-alert('funziona')
+const app = new Vue(
+    {
+        el: '#root',
+
+        data: {
+            errore: '',
+            url: 'https://api.themoviedb.org/3/search/multi?api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=falcon&include_adult=false',
+            arraySearch: [],
+
+        },
+
+        methods: {
+
+
+        },
+
+
+
+        mounted() {
+            axios
+                .get(this.url)
+                .then(resp => {
+                    // console.log(resp.data.results);
+                    this.arraySearch = resp.data.results
+                    console.log(this.arraySearch);
+                })
+
+                .catch(e => {
+                    this.errore = `Huston, We Have a Problem! --- ${e} `
+                })
+
+
+
+
+
+
+        },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    },
+);
