e4868ec2a8ac7518d020173139c92dda9e096161
planeset94 <planeset94@gmail.com>
2021-06-08 16:54:11 +0200

    M2: search already includes tv series

---
+++ Milestone_2/asset/JS/main.js
@@ -0,0 +1,77 @@
+const app = new Vue(
+    {
+        el: '#root',
+
+        data: {
+            errore: '',
+            userSearch: '',
+            partialUrl: 'https://api.themoviedb.org/3/search/multi?api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=',
+            arraySearch: [],
+            visible: true,
+
+        },
+
+        methods: {
+            callApi() {
+                if (this.userSearch == '') {
+                    this.visible = false
+                } else {
+                    this.visible = true
+                    axios
+                        .get(this.partialUrl + this.userSearch)
+                        .then(resp => {
+                            // console.log(resp.data.results);
+                            this.arraySearch = resp.data.results
+                            console.log(this.arraySearch);
+
+                            let listLang = []
+                            this.arraySearch.forEach(lang => {
+                                if (!listLang.includes(lang.original_language)) {
+                                    listLang.push(lang.original_language)
+                                }
+                            })
+                            console.log(listLang);
+
+
+
+
+                        })
+
+                        .catch(e => {
+                            this.errore = `Huston, We Have a Problem! --- ${e} `
+                        })
+
+
+                }
+
+            },
+
+        },
+
+        mounted() {
+
+
+
+
+
+
+        },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    },
+);
