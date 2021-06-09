1081ebc1713ff2d2e471f4996d1f0f666dd800d3
planeset94 <planeset94@gmail.com>
2021-06-09 12:29:57 +0200

    M2: ADJ language

--- Milestone_2/asset/JS/main.js
+++ Milestone_2/asset/JS/main.js
@@ -6,10 +6,32 @@ const app = new Vue(
             errore: '',
             userSearch: '',
             partialUrl: 'https://api.themoviedb.org/3/search/multi?',
-            apiKey: '',
+            apiKey: 'api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=',
             arraySearch: [],
             visible: true,
-            flags: 'https://unpkg.com/language-icons/icons/',
+            flags: 'https://www.countryflags.io/',
+            arrayLang: ['en', 'fr', 'te', 'es', 'bn', 'ja', 'tr', 'de', 'nl', 'eu', 'ar', 'gl', 'it', 'cn', 'pt', 'hu', 'ko', 'cs'],
+            arrayFlags: [
+                'gb',
+                'fr',
+                'in',
+                'es',
+                'bd',
+                'jp',
+                'tr',
+                'de',
+                'bq',
+                'es',
+                'ae',
+                'es',
+                'it',
+                'cn',
+                'pt',
+                'hu',
+                'kr',
+                'cz'
+            ],
+
 
         },
 
@@ -25,7 +47,11 @@ const app = new Vue(
                         .then(resp => {
                             // console.log(resp.data.results);
                             this.arraySearch = resp.data.results
-                            console.log(this.arraySearch)
+                            // console.log(this.arraySearch)
+
+
+
+
 
                             //Messaggio di errore per mancata ricerca
                             if (resp.data.total_results == '') {
@@ -37,13 +63,35 @@ const app = new Vue(
 
 
 
-                            let listLang = []
-                            this.arraySearch.forEach(lang => {
-                                if (!listLang.includes(lang.original_language)) {
-                                    listLang.push(lang.original_language)
+                            // let listLang = []
+                            // this.arraySearch.forEach(lang => {
+                            //     if (!listLang.includes(lang.original_language)) {
+                            //         listLang.push(lang.original_language)
+                            //     }
+                            // })
+                            // console.log(listLang);
+
+
+                            this.arraySearch.forEach(el => {
+                                // console.log(el.original_language);
+                                if (this.arrayLang.indexOf(el.original_language) !== -1) {
+                                    let position = this.arrayLang.indexOf(el.original_language)
+                                    // console.log(this.arrayFlags[position])
+                                    el.bandiera = this.arrayFlags[position]
+                                    // console.log(el);
+
+
+                                } else {
+                                    console.log('lang not found');
+
                                 }
+
+
                             })
-                            console.log(listLang);
+
+
+
+
 
 
 
