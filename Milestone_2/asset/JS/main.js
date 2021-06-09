0fcfc0bf066d94dea9d8b56eda933b9fec9c7f44
planeset94 <planeset94@gmail.com>
2021-06-09 09:53:14 +0200

    M2: ADJ url link

--- Milestone_2/asset/JS/main.js
+++ Milestone_2/asset/JS/main.js
@@ -5,7 +5,8 @@ const app = new Vue(
         data: {
             errore: '',
             userSearch: '',
-            partialUrl: 'https://api.themoviedb.org/3/search/multi?api_key=3e08cf6c1a9102d41852f4ea927dbc55&query=',
+            partialUrl: 'https://api.themoviedb.org/3/search/multi?',
+            apiKey: '',
             arraySearch: [],
             visible: true,
             flags: 'https://unpkg.com/language-icons/icons/',
@@ -20,7 +21,7 @@ const app = new Vue(
                 } else {
                     this.visible = true
                     axios
-                        .get(this.partialUrl + this.userSearch)
+                        .get(this.partialUrl + this.apiKey + this.userSearch)
                         .then(resp => {
                             // console.log(resp.data.results);
                             this.arraySearch = resp.data.results
